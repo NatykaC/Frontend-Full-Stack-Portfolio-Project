bc0dfff07a5505d608d63df7afe642ea41436d6b
Natyka Callwood <Natykacallwood@pursuit.org>
2023-11-17 14:03:50 -0500

    Show page completed with edit and delete buttons on page. Tested show page and each crystal renders when its name is clicked on from the crystals list page. Edit button goes to the edit form page successfully.

--- src/components/CrystalDetails.jsx
+++ src/components/CrystalDetails.jsx
@@ -1,7 +1,95 @@
+import {useParams, useNavigate, Link} from "react-router-dom";
+import {useState, useEffect} from "react";
+
+const API = import.meta.env.VITE_API_URL;
+
 function CrystalDetails() {
+    const [crystal, setCrystal] = useState({
+       name: "",
+       color: "",
+       hardness: "",
+       rarity: "",
+       can_be_water_cleansed: false,
+       healing_effects: "",
+       collected_on: "",
+       favorite: false
+    });
+
+    let {index}=useParams();
+    let navigate = useNavigate();
+
+    useEffect(()=>{
+        const fetchOneCrystal = async()=>{
+            try {
+                fetch(`${API}/crystals/${index}`)
+                .then(res=>res.json())
+                .then(res=>{
+                    setCrystal(res)
+                })
+            } catch (error) {
+                return error
+            }
+        }
+        fetchOneCrystal()
+    }, []);
+
+    const HandleDeleteCrystal = ()=>{
+        try {
+            fetch(`${API}/crystals/${index}`, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(crystal)
+            })
+            .then(res=>res.json())
+            .then(()=>navigate("/crystals"))
+        } catch (error) {
+            return error
+        }
+    };
+
     return (
         <div>
-            <h1>Testing Show page</h1>
+            <div>
+                <h3>
+                    Crystal Name:<br/>{crystal.name}
+                    <br/>
+                    <br/>
+                    Crystal Color:<br/>{crystal.color}
+                    <br/>
+                    <br/>
+                    Crystal Hardness:<br/>{crystal.hardness}
+                    <br/>
+                    <br/>
+                    Crystal Rarity:<br/>{crystal.rarity}
+                    <br/>
+                    <br/>
+                    Can This Crystal Be Cleansed Using Water?<br/>{crystal.can_be_water_cleansed ? <span>💧✅💧</span> : <span>💧❌💧</span>}
+                    <br/>
+                    <br/>
+                    What Are The Healing Effects For This Crystal?<br/>{crystal.healing_effects}
+                    <br/>
+                    <br/>
+                    When Was This Crystal Created?<br/>{crystal.collected_on}
+                    <br/>
+                    <br/>
+                    Is This Crystal One Of Your Favorites?<br/>{crystal.favorite ? <span>❤️</span> : <span>💔</span>} 
+                </h3>
+                <div>
+                    <button>
+                        <Link to={'/crystals'}>Back to My Crystals List</Link>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <Link to={`/crystals/${index}/edit`}>Edit: {crystal.name}</Link>
+                    </button>
+                </div>
+                <div>
+                    <button onClick={HandleDeleteCrystal}>Delete {crystal.name}</button>
+                </div>
+            </div>
         </div>
     )
 };
